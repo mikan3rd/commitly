2535c60e1239fad5c3520c09982a2c34e9690a67
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-05-31 22:34:39 +0900

    refactor: pages/index.jsをts化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className="container">
       <Head>
@@ -200,4 +200,6 @@ export default function Home() {
       `}</style>
     </div>
   );
-}
+};
+
+export default Home;
