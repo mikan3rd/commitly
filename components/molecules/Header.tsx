976b5d69ddc99fc6251964aed4c7fa3907352c07
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-06-30 00:56:14 +0900

    fix: lint対応

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { css } from "emotion";
-
 import Link from "next/link";
 import { Dropdown, Icon, Image } from "semantic-ui-react";
 
