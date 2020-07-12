e6b519537c3e9e1b73d86beee94cbd5927dcfd96
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-07-12 22:02:16 +0900

    fix: createContextの型定義の変更

--- components/molecules/LoginButton.tsx
+++ components/molecules/LoginButton.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
 
+import { useUser } from "../../context/userContext";
+
 export const LoginButton: React.FC = () => {
+  const { user } = useUser();
   return (
     <Button color="black" size="big">
       <Icon name="github" />
-      GitHubログイン
+      {user ? "GitHubログイン中" : "GitHubログイン"}
     </Button>
   );
 };
--- context/userContext.tsx
+++ context/userContext.tsx
@@ -2,31 +2,30 @@ import React from "react";
 
 import firebase from "../utils/clientApp";
 
-export const UserContext = React.createContext({});
+type UserContextType = {
+  user: firebase.User | null;
+  loadingUser: boolean;
+};
+
+export const UserContext = React.createContext<UserContextType>(undefined);
 
 export default function UserContextComp({ children }) {
   const [user, setUser] = React.useState<firebase.User | null>(null);
   const [loadingUser, setLoadingUser] = React.useState(true); // Helpful, to update the UI accordingly.
 
   React.useEffect(() => {
-    // Listen authenticated user
     const unsubscriber = firebase.auth().onAuthStateChanged(async (user) => {
-      try {
-        if (user) {
-          setUser(user);
-        } else setUser(null);
-      } catch (error) {
-        // Most probably a connection error. Handle appropriately.
-      } finally {
-        setLoadingUser(false);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
       }
+      setLoadingUser(false);
     });
-
-    // Unsubscribe auth listener on unmount
     return () => unsubscriber();
   }, []);
 
-  return <UserContext.Provider value={{ user, setUser, loadingUser }}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ user, loadingUser }}>{children}</UserContext.Provider>;
 }
 
 // Custom hook that shorhands the context!
