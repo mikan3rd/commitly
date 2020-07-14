04ae8f4d10d67ccde9572cd7cc02967dd702fcd1
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-07-15 00:33:18 +0900

    feat: firebaseログインの処理実装

--- context/userContext.tsx
+++ context/userContext.tsx
@@ -1,31 +1,100 @@
 import React from "react";
+import Router from "next/router";
+import { toast } from "react-semantic-toasts";
 
 import firebase from "../utils/clientApp";
+import { User } from "../models/User";
 
 type UserContextType = {
   user: firebase.User | null;
+  userDoc: User | null;
   loadingUser: boolean;
+  login: () => void;
+};
+
+type GithubCredentialType = {
+  credential: { accessToken: string };
+  additionalUserInfo: { username: string; isNewUser: boolean; profile: { id: number } };
+  user: { uid: string };
+};
+
+type TwitterCredentialType = {
+  credential: { accessToken: string; secret: string };
+  additionalUserInfo: { username: string; isNewUser: boolean; profile: { id_str: string } };
+  user: { uid: string };
 };
 
 export const UserContext = React.createContext<UserContextType>(undefined);
 
 export default function UserContextComp({ children }) {
   const [user, setUser] = React.useState<firebase.User | null>(null);
-  const [loadingUser, setLoadingUser] = React.useState(true); // Helpful, to update the UI accordingly.
+  const [loadingUser, setLoadingUser] = React.useState(true);
+  const [userDoc, setUserDoc] = React.useState<User | null>(null);
 
-  React.useEffect(() => {
-    const unsubscriber = firebase.auth().onAuthStateChanged(async (user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
+  const getUserDoc = async (uid: string) => {
+    const doc = await firebase.firestore().collection("users").doc(uid).get();
+    const params = doc.data() as { [field: string]: unknown };
+    return new User(params);
+  };
+
+  const reloadUserDoc = async (uid: string) => {
+    const userDoc = await getUserDoc(uid);
+    setUserDoc(userDoc);
+  };
+
+  const updateFirestoreUserDoc = async (uid: string, userDoc: User) => {
+    await firebase.firestore().collection("users").doc(uid).set(userDoc.getFirestoreObject(), { merge: true });
+  };
+
+  const setCurrentUser = async () => {
+    setLoadingUser(true);
+    firebase.auth().onAuthStateChanged(async (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        await reloadUserDoc(currentUser.uid);
       }
       setLoadingUser(false);
     });
-    return () => unsubscriber();
+  };
+
+  const login = async () => {
+    const provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope("read:user");
+
+    const userCredential: unknown = await firebase.auth().signInWithPopup(provider);
+
+    // フルスクリーンの場合はポップアップで新規タブを開いてネットワークがオフラインになるため必要
+    await firebase.firestore().enableNetwork();
+
+    const {
+      credential: { accessToken },
+      additionalUserInfo: {
+        username,
+        profile: { id },
+      },
+      user: { uid },
+    } = userCredential as GithubCredentialType;
+
+    const userDoc = await getUserDoc(uid);
+    const nextUserDoc = userDoc.setGithub({ username, userId: String(id), accessToken });
+    await updateFirestoreUserDoc(uid, nextUserDoc);
+    await setCurrentUser();
+
+    await Router.push("/setting");
+
+    setTimeout(() => {
+      toast({
+        type: "success",
+        title: "ログインしました！",
+      });
+    }, 500);
+  };
+
+  React.useEffect(() => {
+    setCurrentUser();
   }, []);
 
-  return <UserContext.Provider value={{ user, loadingUser }}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ user, userDoc, loadingUser, login }}>{children}</UserContext.Provider>;
 }
 
 // Custom hook that shorhands the context!
