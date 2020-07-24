a6923a86d62dccb06082f7f9c85f87fef46dd0b3
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-07-24 20:06:37 +0900

    chore: firebaseの設定追記

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,13 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const functions = require("firebase-functions");
-const next = require("next");
+import * as functions from "firebase-functions";
+import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, conf: { distDir: "next" } });
 const handle = app.getRequestHandler();
 
-exports.next = functions.https.onRequest(async (req, res) => {
-  console.log("File: " + req.originalUrl); // log the page.js file that is being requested
+export const nextApp = functions.https.onRequest(async (req, res) => {
+  console.log("File: " + req.originalUrl);
   await app.prepare();
   handle(req, res);
 });
