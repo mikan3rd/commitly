dbe69476c22708ee94580908288cf09ddfc7d848
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-07-25 00:46:11 +0900

    chore: devモードを削除

--- firebaseFunctions.js
+++ firebaseFunctions.js
@@ -4,11 +4,9 @@ const { join } = require("path");
 const functions = require("firebase-functions");
 const { default: next } = require("next");
 
-const isDev = process.env.NODE_ENV !== "production";
 const nextjsDistDir = join("src", require("./src/next.config.js").distDir);
 
 const nextjsServer = next({
-  dev: isDev,
   conf: {
     distDir: nextjsDistDir,
   },
