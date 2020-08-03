174fce0a47ec969c359122ba7b06f1406b43fd90
mikan3rd <mikan.no.kikoushi@gmail.com>
2020-08-03 21:29:00 +0900

    search console用のタグ追加

--- app/components/templates/Meta.tsx
+++ app/components/templates/Meta.tsx
@@ -20,6 +20,7 @@ export const Meta = React.memo<{
       <meta name="viewport" content="initial-scale=1, width=device-width" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" type="image/png" href="/logo_icon.png" />
+      <meta name="google-site-verification" content="lhrIBgvKmE5lXsXKci8nY3wNNFe4PjfaLY6aZF7UjUM" />
     </Head>
   );
 });
